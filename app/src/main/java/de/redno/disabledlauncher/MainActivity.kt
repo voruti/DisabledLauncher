69abb4b2a7031b97aa62ec9055668a8a5e0155fc
voruti <45130096+voruti@users.noreply.github.com>
2023-02-14 23:00:58 +0100

    Clear search

--- app/src/main/java/de/redno/disabledlauncher/MainActivity.kt
+++ app/src/main/java/de/redno/disabledlauncher/MainActivity.kt
@@ -14,11 +14,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -176,7 +179,7 @@ fun AppEntry(appEntry: AppEntryInList, modifier: Modifier = Modifier) {
 fun AppList(packageNameList: List<String>, modifier: Modifier = Modifier) {
     val context = LocalContext.current
 
-    var text by remember { mutableStateOf("") }
+    var text by rememberSaveable { mutableStateOf("") }
     val appEntryList =
         packageNameList.map { getDetailsForPackage(context, it) } // TODO: prevent being called on every text change
 
@@ -184,8 +187,19 @@ fun AppList(packageNameList: List<String>, modifier: Modifier = Modifier) {
         TextField( // TODO: https://developer.android.com/jetpack/compose/text#enter-modify-text
             value = text,
             onValueChange = { text = it },
-            placeholder = { Text("Search for apps...") },
-            modifier = Modifier.fillMaxWidth()
+            label = { Text("Search") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            trailingIcon = {
+                IconButton(
+                    onClick = { text = "" }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear search"
+                    )
+                }
+            }
         )
         LazyColumn {
             items(items = appEntryList.filter {
