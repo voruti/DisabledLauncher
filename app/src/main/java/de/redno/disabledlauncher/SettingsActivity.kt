3adcacfb040b2ecff595447b2591772213424e41
voruti <45130096+voruti@users.noreply.github.com>
2023-02-21 17:54:24 +0100

    Add fallbackToGooglePlay switch

--- app/src/main/java/de/redno/disabledlauncher/SettingsActivity.kt
+++ app/src/main/java/de/redno/disabledlauncher/SettingsActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Shop
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat.startActivityForResult
 import de.redno.disabledlauncher.common.ListEntry
@@ -98,15 +100,34 @@ fun SettingsPreview() {
 @Composable
 fun SettingsList(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences("de.redno.disabledlauncher", Context.MODE_PRIVATE)
 
     Column(modifier = modifier) {
-        val currentUri = context.getSharedPreferences("de.redno.disabledlauncher", Context.MODE_PRIVATE)
-            .getString("launchableAppsFile", null)
+        val currentUri = sharedPreferences.getString("launchableAppsFile", null)
             ?: "Click to choose a file"
         ListEntry(
             title = "Launchable apps file",
             description = currentUri,
             modifier = Modifier.clickable { pickLaunchableAppsFile() }
+        )
+        val initialFallbackToGooglePlay = sharedPreferences.getBoolean("fallbackToGooglePlay", false)
+        var fallbackToGooglePlay by remember { mutableStateOf(initialFallbackToGooglePlay) }
+        ListEntry(
+            icon = { Icon(Icons.Default.Shop, "Store icon") },
+            title = "Fallback to Google Play",
+            description = "When ADB/Shizuku isn't available, redirect the user to Google Play",
+            endContent = { Switch(checked = fallbackToGooglePlay, onCheckedChange = null) },
+            modifier = Modifier.toggleable(
+                role = Role.Switch,
+                value = fallbackToGooglePlay,
+                onValueChange = {
+                    sharedPreferences.edit()
+                        .putBoolean("fallbackToGooglePlay", it)
+                        .apply()
+
+                    fallbackToGooglePlay = it
+                }
+            )
         )
     }
 }
