db7d6d6fd15886edf0b8a6bfb4f0413398c9aa7a
voruti <45130096+voruti@users.noreply.github.com>
2023-02-26 00:07:54 +0100

    Extract getLaunchableAppsFileUri

--- app/src/main/java/de/redno/disabledlauncher/service/Datasource.kt
+++ app/src/main/java/de/redno/disabledlauncher/service/Datasource.kt
@@ -7,55 +7,57 @@ import kotlin.math.floor
 object Datasource {
 
     fun loadAppList(context: Context): List<String> {
-        context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-            .getString("launchableAppsFile", null)?.let { uri ->
+        getLaunchableAppsFileUri(context)?.let { uri ->
 
-                FileService.readFile(context, uri, MainFile::class.java)
-                    ?.let { return it.packages }
-            }
+            FileService.readFile(context, uri, MainFile::class.java)
+                ?.let { return it.packages }
+        }
 
         return emptyList()
     }
 
     fun raisePackage(context: Context, packageName: String): Boolean {
-        context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-            .getString("launchableAppsFile", null)?.let { uri ->
+        getLaunchableAppsFileUri(context)?.let { uri ->
 
-                FileService.readFile(context, uri, MainFile::class.java)?.let {
-                    val oldIndex = it.packages.indexOf(packageName)
-                    if (oldIndex <= 0) {
-                        return false
-                    }
-
-                    return FileService.writeFile(context, uri, MainFile(
-                        it.packages
-                            .filter { it != packageName }
-                            .toMutableList()
-                            .also {
-                                it.add(
-                                    floor((oldIndex * 2) / 3f).toInt(),
-                                    packageName
-                                )
-                            }
-                    ))
+            FileService.readFile(context, uri, MainFile::class.java)?.let {
+                val oldIndex = it.packages.indexOf(packageName)
+                if (oldIndex <= 0) {
+                    return false
                 }
+
+                return FileService.writeFile(context, uri, MainFile(
+                    it.packages
+                        .filter { it != packageName }
+                        .toMutableList()
+                        .also {
+                            it.add(
+                                floor((oldIndex * 2) / 3f).toInt(),
+                                packageName
+                            )
+                        }
+                ))
             }
+        }
 
         return false
     }
 
     fun removePackage(context: Context, packageName: String): Boolean {
-        context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-            .getString("launchableAppsFile", null)?.let { uri ->
+        getLaunchableAppsFileUri(context)?.let { uri ->
 
-                FileService.readFile(context, uri, MainFile::class.java)?.let {
-                    return FileService.writeFile(
-                        context, uri, MainFile(
-                            it.packages.filter { it != packageName })
-                    )
-                }
+            FileService.readFile(context, uri, MainFile::class.java)?.let {
+                return FileService.writeFile(
+                    context, uri, MainFile(
+                        it.packages.filter { it != packageName })
+                )
             }
+        }
 
         return false
+    }
+
+    private fun getLaunchableAppsFileUri(context: Context): String? {
+        return context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+            .getString("launchableAppsFile", null)
     }
 }
