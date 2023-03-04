285eaba1d5701c4fe7356630c7486da1b7ee9c45
voruti <45130096+voruti@users.noreply.github.com>
2023-03-04 22:50:34 +0100

    Default internal file

--- app/src/main/java/de/redno/disabledlauncher/service/Datasource.kt
+++ app/src/main/java/de/redno/disabledlauncher/service/Datasource.kt
@@ -1,23 +1,28 @@
 package de.redno.disabledlauncher.service
 
 import android.content.Context
+import android.widget.Toast
+import de.redno.disabledlauncher.R
+import de.redno.disabledlauncher.asyncToastMakeText
 import de.redno.disabledlauncher.model.file.MainFile
 import kotlin.math.floor
 
 object Datasource {
+    const val INTERNAL_MAIN_FILE = "internalMainFile"
 
     fun loadAppList(context: Context): List<String> {
-        getLaunchableAppsFileUri(context)?.let { uri ->
+        getLaunchableAppsFileUri(context).let { uri ->
 
             FileService.readFile(context, uri, MainFile::class.java)
                 ?.let { return it.packages }
         }
 
+        asyncToastMakeText(context, context.getString(R.string.couldnt_load_app_list), Toast.LENGTH_SHORT)
         return emptyList()
     }
 
     fun raisePackage(context: Context, packageName: String): Boolean {
-        getLaunchableAppsFileUri(context)?.let { uri ->
+        getLaunchableAppsFileUri(context).let { uri ->
 
             FileService.readFile(context, uri, MainFile::class.java)?.let {
                 val oldIndex = it.packages.indexOf(packageName)
@@ -43,9 +48,14 @@ object Datasource {
     }
 
     fun removePackage(context: Context, packageName: String): Boolean {
-        getLaunchableAppsFileUri(context)?.let { uri ->
+        getLaunchableAppsFileUri(context).let { uri ->
 
             FileService.readFile(context, uri, MainFile::class.java)?.let {
+                // test for existance beforehand:
+                if (!it.packages.contains(packageName)) {
+                    return false
+                }
+
                 return FileService.writeFile(
                     context, uri, MainFile(
                         it.packages.filter { it != packageName })
@@ -57,7 +67,7 @@ object Datasource {
     }
 
     fun addPackages(context: Context, packageNameList: List<String>): Boolean {
-        getLaunchableAppsFileUri(context)?.let { uri ->
+        getLaunchableAppsFileUri(context).let { uri ->
 
             FileService.readFile(context, uri, MainFile::class.java)?.let {
                 return FileService.writeFile(
@@ -74,8 +84,8 @@ object Datasource {
         return false
     }
 
-    private fun getLaunchableAppsFileUri(context: Context): String? {
+    private fun getLaunchableAppsFileUri(context: Context): String {
         return context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-            .getString("launchableAppsFile", null)
+            .getString("launchableAppsFile", INTERNAL_MAIN_FILE)!!
     }
 }
--- app/src/main/java/de/redno/disabledlauncher/service/FileService.kt
+++ app/src/main/java/de/redno/disabledlauncher/service/FileService.kt
@@ -4,24 +4,50 @@ import android.content.Context
 import android.net.Uri
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.io.FileNotFoundException
 import java.io.IOException
 
 object FileService {
+    private const val MAIN_FILE_NAME = "mainFile.json"
+
     private val objectMapper = jacksonObjectMapper()
         .enable(SerializationFeature.INDENT_OUTPUT)
 
     fun <T> readFile(context: Context, uri: String, clazz: Class<T>): T? {
+        if (uri == Datasource.INTERNAL_MAIN_FILE) {
+            touchInternalFile(context, MAIN_FILE_NAME, "{\"packages\":[]}")
+        }
+
         try {
             // open & read file:
-            return context.contentResolver.openInputStream(Uri.parse(uri))
-                ?.use {
-                    it.bufferedReader()
-                        .use { it.readText() }
+            val fileContent = if (uri == Datasource.INTERNAL_MAIN_FILE) {
+                context.openFileInput(MAIN_FILE_NAME)
+                    .bufferedReader().use {
+                        it.readText()
+                    }
+            } else {
+                context.contentResolver.openInputStream(Uri.parse(uri))?.use {
+                    it.bufferedReader().use {
+                        it.readText()
+                    }
                 }
+            }
+            return fileContent
                 // deserialize into object:
                 ?.let { return objectMapper.readValue(it, clazz) }
         } catch (e: IOException) {
             return null
+        }
+    }
+
+    private fun touchInternalFile(context: Context, fileName: String, content: String) {
+        try {
+            context.openFileInput(fileName).close()
+        } catch (_: FileNotFoundException) {
+            // file doesn't exist, creating it:
+            context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
+                it.write(content.toByteArray())
+            }
         }
     }
 
@@ -31,10 +57,17 @@ object FileService {
             val jsonString = objectMapper.writeValueAsString(obj)
 
             // open & write file:
-            context.contentResolver.openOutputStream(Uri.parse(uri), "wt")?.use {
-                it.bufferedWriter().use {
-                    it.write(jsonString)
+            if (uri == Datasource.INTERNAL_MAIN_FILE) {
+                context.openFileOutput(MAIN_FILE_NAME, Context.MODE_PRIVATE).use {
+                    it.write(jsonString.toByteArray())
                     return true
+                }
+            } else {
+                context.contentResolver.openOutputStream(Uri.parse(uri), "wt")?.use {
+                    it.bufferedWriter().use {
+                        it.write(jsonString)
+                        return true
+                    }
                 }
             }
         } catch (_: IOException) {
