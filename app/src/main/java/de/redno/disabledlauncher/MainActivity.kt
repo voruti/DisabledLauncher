d3595706221226ba098a17256594be582d70cee0
voruti <45130096+voruti@users.noreply.github.com>
2023-03-26 23:20:06 +0200

    Introduce DirectLauncherPreview

--- app/src/main/java/de/redno/disabledlauncher/MainActivity.kt
+++ app/src/main/java/de/redno/disabledlauncher/MainActivity.kt
@@ -7,44 +7,29 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AppBlocking
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asAndroidBitmap
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.pm.ShortcutInfoCompat
-import androidx.core.content.pm.ShortcutManagerCompat.isRequestPinShortcutSupported
-import androidx.core.content.pm.ShortcutManagerCompat.requestPinShortcut
-import androidx.core.graphics.drawable.IconCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import de.redno.disabledlauncher.common.AndroidUtil
-import de.redno.disabledlauncher.model.*
-import de.redno.disabledlauncher.model.exception.*
+import de.redno.disabledlauncher.model.exception.DisabledLauncherException
 import de.redno.disabledlauncher.service.AppService
 import de.redno.disabledlauncher.service.Datasource
-import de.redno.disabledlauncher.ui.components.ListItem
-import de.redno.disabledlauncher.ui.components.ToolbarComponent
+import de.redno.disabledlauncher.ui.screens.DirectLauncherScreen
 import de.redno.disabledlauncher.ui.screens.SettingsScreen
 import de.redno.disabledlauncher.ui.theme.DisabledLauncherTheme
 
 class MainActivity : ComponentActivity() { // TODO: faster startup somehow?
     companion object {
+        const val ROUTE_DIRECT_LAUNCHER = "directlauncher"
+        const val ROUTE_SETTINGS = "settings"
+
         var lastObject: MainActivity? = null
 
         fun exit() {
@@ -123,14 +108,11 @@ class MainActivity : ComponentActivity() { // TODO: faster startup somehow?
 }
 
 
-const val ROUTE_DIRECT_LAUNCHER = "directlauncher"
-const val ROUTE_SETTINGS = "settings"
-
 @Composable
 fun DisabledLauncherNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = ROUTE_DIRECT_LAUNCHER
+    startDestination: String = MainActivity.ROUTE_DIRECT_LAUNCHER
 ) {
     val context = LocalContext.current
 
@@ -139,185 +121,14 @@ fun DisabledLauncherNavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        composable(ROUTE_DIRECT_LAUNCHER) {
-            Scaffold(
-                topBar = {
-                    ToolbarComponent(
-                        title = stringResource(id = R.string.app_name),
-                        onSettingsClick = { navController.navigate(ROUTE_SETTINGS) }
-                    )
-                },
-                floatingActionButton = {
-                    FloatingActionButton(onClick = Thread {
-                        try {
-                            AppService.disableAllApps(context)
-                        } catch (e: DisabledLauncherException) {
-                            e.getLocalizedMessage(context)?.let {
-                                AndroidUtil.asyncToastMakeText(context, it, Toast.LENGTH_SHORT)
-                            }
-                        }
-                    }::start) {
-                        Icon(
-                            Icons.Default.AppBlocking,
-                            contentDescription = stringResource(R.string.disable_all_apps)
-                        )
-                    }
-                }
-            ) {
-                AppList(Datasource.loadAppList(context), Modifier.padding(it))
-            }
+        composable(MainActivity.ROUTE_DIRECT_LAUNCHER) {
+            DirectLauncherScreen(
+                { navController.navigate(MainActivity.ROUTE_SETTINGS) },
+                Datasource.loadAppList(context)
+            )
         }
-        composable(ROUTE_SETTINGS) {
+        composable(MainActivity.ROUTE_SETTINGS) {
             SettingsScreen()
         }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun AppEntry(app: App, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val sharedPreferences = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
-
-    var dropdownExpanded by remember { mutableStateOf(false) }
-    ListItem(
-        icon = {
-            Image(
-                app.icon.asImageBitmap(),
-                String.format(stringResource(R.string.app_icon), app.name)
-            )
-        },
-        title = app.name,
-        description = app.packageName,
-        italicStyle = !app.isEnabled,
-        disabledStyle = !app.isInstalled,
-        contextContent = {
-            DropdownMenu(
-                expanded = dropdownExpanded,
-                onDismissRequest = { dropdownExpanded = false }
-            ) {
-                DropdownMenuItem(onClick = {
-                    if (isRequestPinShortcutSupported(context)) {
-                        val intent = Intent(context, ActionReceiverActivity::class.java)
-                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                            .setAction("${context.packageName}.action.OPEN_APP")
-                            .putExtra("package_name", app.packageName)
-                        val shortcutInfo = ShortcutInfoCompat.Builder(context, app.packageName)
-                            .setShortLabel(app.name)
-                            .setLongLabel(app.name)
-                            .setIcon(IconCompat.createWithBitmap(app.icon.asImageBitmap().asAndroidBitmap()))
-                            .setIntent(intent)
-                            .build()
-                        requestPinShortcut(context, shortcutInfo, null)
-                    } else {
-                        Toast.makeText(
-                            context,
-                            context.getString(R.string.launcher_not_support_pinned),
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-
-                    dropdownExpanded = false
-                }) {
-                    Text(stringResource(R.string.add_shortcut))
-                }
-                DropdownMenuItem(onClick = {
-                    if (Datasource.removePackage(context, app.packageName)) {
-                        dropdownExpanded = false
-
-                        AndroidUtil.asyncToastMakeText(
-                            context,
-                            String.format(context.getString(R.string.app_removed), app.name),
-                            Toast.LENGTH_SHORT
-                        )
-                        // TODO: refresh app list (+ there are other actions/code locations that need to trigger a list refresh)
-                    } else {
-                        Toast.makeText(context, context.getString(R.string.couldnt_remove_app), Toast.LENGTH_LONG)
-                            .show()
-                    }
-                }) {
-                    Text(stringResource(R.string.remove_app))
-                }
-            }
-        },
-        modifier = modifier.combinedClickable(
-            onClick = {
-                Thread {
-                    if (app.isInstalled) {
-                        try {
-                            AppService.openAppLogic(context, app)
-                            if (sharedPreferences.getBoolean("sortAppsByUsage", false)) {
-                                Datasource.raisePackage(context, app.packageName)
-                            }
-                            MainActivity.exit()
-                        } catch (e: DisabledLauncherException) {
-                            e.getLocalizedMessage(context)?.let {
-                                AndroidUtil.asyncToastMakeText(context, it, Toast.LENGTH_SHORT)
-                            }
-                        }
-                    }
-                }.start()
-            },
-            onLongClick = { dropdownExpanded = true }
-        )
-    )
-}
-
-@Composable
-fun AppList(packageNameList: List<String>, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-
-    Column(modifier = modifier) {
-        var text by rememberSaveable { mutableStateOf("") }
-        TextField( // TODO: https://developer.android.com/jetpack/compose/text#enter-modify-text
-            value = text, // TODO: put into toolbar
-            onValueChange = { text = it },
-            label = { Text(stringResource(id = R.string.search)) },
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
-            trailingIcon = {
-                IconButton(
-                    onClick = { text = "" }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Clear,
-                        contentDescription = context.getString(R.string.clear_search)
-                    )
-                }
-            }
-        )
-        LazyColumn {
-            val appList = packageNameList.map {
-                AppService.getDetailsForPackage(context, it)
-            } // TODO: prevent being called on every text change
-            items(items = appList
-                .filter {
-                    val searchTerms = text.trim().lowercase().split(" ")
-                    val searchReference = "${it.name.trim().lowercase()} ${it.packageName.trim().lowercase()}"
-
-                    searchTerms.all { searchReference.contains(it) }
-                }
-                .sortedBy { !it.isInstalled },
-                key = App::packageName
-            ) { AppEntry(it) }
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    DisabledLauncherTheme {
-        Scaffold(
-            topBar = { ToolbarComponent(title = stringResource(id = R.string.app_name), onSettingsClick = {}) },
-            floatingActionButton = {
-                FloatingActionButton(onClick = {}) {
-                    Icon(Icons.Default.AppBlocking, contentDescription = null)
-                }
-            },
-            content = {
-                AppList(listOf("de.test.1", "de.test.2", "de.test.3"), Modifier.padding(it))
-            }
-        )
     }
 }
---
+++ app/src/main/java/de/redno/disabledlauncher/ui/screens/DirectLauncherScreen.kt
@@ -0,0 +1,216 @@
+package de.redno.disabledlauncher.ui.screens
+
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AppBlocking
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import de.redno.disabledlauncher.ActionReceiverActivity
+import de.redno.disabledlauncher.MainActivity
+import de.redno.disabledlauncher.R
+import de.redno.disabledlauncher.common.AndroidUtil
+import de.redno.disabledlauncher.model.App
+import de.redno.disabledlauncher.model.exception.DisabledLauncherException
+import de.redno.disabledlauncher.service.AppService
+import de.redno.disabledlauncher.service.Datasource
+import de.redno.disabledlauncher.ui.components.ListItem
+import de.redno.disabledlauncher.ui.components.ToolbarComponent
+import de.redno.disabledlauncher.ui.theme.DisabledLauncherTheme
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun DirectLauncherPreview() {
+    DisabledLauncherTheme {
+        DirectLauncherScreen({}, listOf("de.test.1", "de.test.2", "de.test.3"))
+    }
+}
+
+
+@Composable
+fun DirectLauncherScreen(
+    onSettingsClick: () -> Unit,
+    packageNameList: List<String>,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            ToolbarComponent(
+                title = stringResource(id = R.string.app_name),
+                onSettingsClick = onSettingsClick
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = Thread {
+                try {
+                    AppService.disableAllApps(context)
+                } catch (e: DisabledLauncherException) {
+                    e.getLocalizedMessage(context)?.let {
+                        AndroidUtil.asyncToastMakeText(context, it, Toast.LENGTH_SHORT)
+                    }
+                }
+            }::start) {
+                Icon(
+                    Icons.Default.AppBlocking,
+                    contentDescription = stringResource(R.string.disable_all_apps)
+                )
+            }
+        }
+    ) {
+        AppList(packageNameList, Modifier.padding(it))
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun AppEntry(app: App, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+
+    var dropdownExpanded by remember { mutableStateOf(false) }
+    ListItem(
+        icon = {
+            Image(
+                app.icon.asImageBitmap(),
+                String.format(stringResource(R.string.app_icon), app.name)
+            )
+        },
+        title = app.name,
+        description = app.packageName,
+        italicStyle = !app.isEnabled,
+        disabledStyle = !app.isInstalled,
+        contextContent = {
+            DropdownMenu(
+                expanded = dropdownExpanded,
+                onDismissRequest = { dropdownExpanded = false }
+            ) {
+                DropdownMenuItem(onClick = {
+                    if (ShortcutManagerCompat.isRequestPinShortcutSupported(context)) {
+                        val intent = Intent(context, ActionReceiverActivity::class.java)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                            .setAction("${context.packageName}.action.OPEN_APP")
+                            .putExtra("package_name", app.packageName)
+                        val shortcutInfo = ShortcutInfoCompat.Builder(context, app.packageName)
+                            .setShortLabel(app.name)
+                            .setLongLabel(app.name)
+                            .setIcon(IconCompat.createWithBitmap(app.icon.asImageBitmap().asAndroidBitmap()))
+                            .setIntent(intent)
+                            .build()
+                        ShortcutManagerCompat.requestPinShortcut(context, shortcutInfo, null)
+                    } else {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.launcher_not_support_pinned),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                    dropdownExpanded = false
+                }) {
+                    Text(stringResource(R.string.add_shortcut))
+                }
+                DropdownMenuItem(onClick = {
+                    if (Datasource.removePackage(context, app.packageName)) {
+                        dropdownExpanded = false
+
+                        AndroidUtil.asyncToastMakeText(
+                            context,
+                            String.format(context.getString(R.string.app_removed), app.name),
+                            Toast.LENGTH_SHORT
+                        )
+                        // TODO: refresh app list (+ there are other actions/code locations that need to trigger a list refresh)
+                    } else {
+                        Toast.makeText(context, context.getString(R.string.couldnt_remove_app), Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }) {
+                    Text(stringResource(R.string.remove_app))
+                }
+            }
+        },
+        modifier = modifier.combinedClickable(
+            onClick = {
+                Thread {
+                    if (app.isInstalled) {
+                        try {
+                            AppService.openAppLogic(context, app)
+                            if (sharedPreferences.getBoolean("sortAppsByUsage", false)) {
+                                Datasource.raisePackage(context, app.packageName)
+                            }
+                            MainActivity.exit()
+                        } catch (e: DisabledLauncherException) {
+                            e.getLocalizedMessage(context)?.let {
+                                AndroidUtil.asyncToastMakeText(context, it, Toast.LENGTH_SHORT)
+                            }
+                        }
+                    }
+                }.start()
+            },
+            onLongClick = { dropdownExpanded = true }
+        )
+    )
+}
+
+@Composable
+private fun AppList(packageNameList: List<String>, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Column(modifier = modifier) {
+        var text by rememberSaveable { mutableStateOf("") }
+        TextField( // TODO: https://developer.android.com/jetpack/compose/text#enter-modify-text
+            value = text, // TODO: put into toolbar
+            onValueChange = { text = it },
+            label = { Text(stringResource(id = R.string.search)) },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            trailingIcon = {
+                IconButton(
+                    onClick = { text = "" }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = context.getString(R.string.clear_search)
+                    )
+                }
+            }
+        )
+        LazyColumn {
+            val appList = packageNameList.map {
+                AppService.getDetailsForPackage(context, it)
+            } // TODO: prevent being called on every text change
+            items(items = appList
+                .filter {
+                    val searchTerms = text.trim().lowercase().split(" ")
+                    val searchReference = "${it.name.trim().lowercase()} ${it.packageName.trim().lowercase()}"
+
+                    searchTerms.all { searchReference.contains(it) }
+                }
+                .sortedBy { !it.isInstalled },
+                key = App::packageName
+            ) { AppEntry(it) }
+        }
+    }
+}
